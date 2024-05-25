dc006d1a0cb0717392c566ce7a0c869e4d2dacd4
Vadimgrig2012 <Vadim.grig2012@gmail.com>
2024-05-25 18:41:09 +0400

    add adaptive

---
+++ js/main-script.js
@@ -0,0 +1,3 @@
+// import log from './modules/svg_header_adaptive.js';
+
+// log();
---
+++ js/modules/svg_header_adaptive.js
@@ -0,0 +1,20 @@
+// const svg = document.querySelector('.main-svg');
+
+// function resizeSVG() {
+//   if (window.innerWidth <= 1024) {
+//     svg.setAttribute('width', '76.7rem');
+//     svg.setAttribute('height', '76.8rem');
+//   } else {
+//     svg.setAttribute('width', '139.6rem');
+//     svg.setAttribute('height', '101rem');
+//   }
+// }
+
+// window.addEventListener('load', resizeSVG);
+// window.addEventListener('resize', resizeSVG);
+
+  
+//   // Экспорт функции log
+// export default resizeSVG;
+
+
