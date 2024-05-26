7a79414c8d045afe239318f6c587de371ac6b704
Vadim Snopov <vadim.grig2012@gmail.com>
2024-05-26 20:40:48 +0400

    add adaptive, add js

---
+++ js/modules/draw_unicorn_anim.js
@@ -0,0 +1,12 @@
+// const svgPath = document.getElementById('Unicorn');
+// const animateBtn = document.getElementById('animate-btn'); 
+
+// let tl = gsap.timeline({ paused: true });
+
+// tl.to(svgPath, { duration: 2, strokeDashoffset: 0, ease: 'power2.inOut' });
+
+// animateBtn.addEventListener('click', () => {
+//     tl.restart();
+// });
+
+// Нужно добавить в конце path свойства stroke-dasharray="1000" stroke-dashoffset="1000"
