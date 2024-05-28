0c5a84b6221a53df98b508b56ac1b2c289f2ed3b
Vadim Snopov <vadim.grig2012@gmail.com>
2024-05-29 02:07:43 +0400

    finaly ball-scene animation

--- js/ball_composition_anim.js
+++ js/ball_composition_anim.js
@@ -4,26 +4,124 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 
 gsap.registerPlugin(ScrollTrigger,MotionPathPlugin);
 
+let mm = gsap.matchMedia();
 
-const pathElement = document.getElementById("pathway2");
+mm.add("(min-width: 1025px)", () => {
 
-const ballPurple = document.getElementById("ball-purple");
-const ballWhite2 = document.getElementById("ball-white2");
+    const pathElement1 = document.getElementById("pathway1");
+
+    const ballPurple = document.getElementById("ball-purple");
+    const ballWhite = document.getElementById("ball-white");
+    const ballWhite2 = document.getElementById("ball-white2");
+    const ballCyanSmall = document.getElementById("ball-cyan-small");
+    const ballRedBig = document.getElementById("ball-red-big");
+    const ballRedSmall = document.getElementById("ball-red-small");
 
 
-gsap.to(ballPurple, {
-    motionPath: {
-        path: pathElement,
-        align: pathElement,
-        autoRotate: false,
-        offsetX: -40,
-        offsetY: -30,
-    },
-    scrollTrigger: {
-        trigger: '#ball-orange-main',
-        start: '-20% center',
-        end: '120% center',
-        scrub: 5,
-        markers: true,
-    }
+    gsap.to(ballPurple, {
+        motionPath: {
+            path: pathElement1,
+            align: pathElement1,
+            autoRotate: false,
+            offsetX: -40,
+            offsetY: -30,
+        },
+        scrollTrigger: {
+            trigger: '#ball-orange-main',
+            start: '-20% center',
+            end: '120% center',
+            scrub: 5,
+        }
+    });
+
+
+    gsap.to(ballWhite, {
+        motionPath: {
+            path: pathElement1,
+            align: pathElement1,
+            start: -0.5,
+            end: -0.3,
+            autoRotate: false,
+            offsetX: -20,
+            offsetY: -10,
+        },
+        scrollTrigger: {
+            trigger: '#ball-orange-main',
+            start: '-20% center',
+            end: '120% center',
+            scrub: 5,
+        }
+    });
+
+    gsap.to(ballWhite2, {
+        motionPath: {
+            path: pathElement1,
+            align: pathElement1,
+            start: -0.3,
+            end: -0.9,
+            autoRotate: false,
+            offsetX: -20,
+            offsetY: -10,
+        },
+        scrollTrigger: {
+            trigger: '#ball-orange-main',
+            start: '-20% center',
+            end: '120% center',
+            scrub: 5,
+        }
+    });
+
+    gsap.to(ballCyanSmall, {
+        motionPath: {
+            path: pathElement1,
+            align: pathElement1,
+            start: 0.6,
+            end: 0.9,
+            autoRotate: false,
+            offsetX: -40,
+            offsetY: -30,
+        },
+        scrollTrigger: {
+            trigger: '#ball-orange-main',
+            start: '-20% center',
+            end: '120% center',
+            scrub: 5,
+        }
+    });
+
+    gsap.to(ballRedBig, {
+        motionPath: {
+            path: pathElement1,
+            align: pathElement1,
+            start: -0.1,
+            end: -0.6,
+            autoRotate: false,
+            offsetX: -60,
+            offsetY: -70,
+        },
+        scrollTrigger: {
+            trigger: '#ball-orange-main',
+            start: '-20% center',
+            end: '120% center',
+            scrub: 5,
+        }
+    });
+
+    gsap.to(ballRedSmall, {
+        motionPath: {
+            path: pathElement1,
+            align: pathElement1,
+            start: 0.3,
+            end: 0.8,
+            autoRotate: false,
+            offsetX: -60,
+            offsetY: -70,
+        },
+        scrollTrigger: {
+            trigger: '#ball-orange-main',
+            start: '-20% center',
+            end: '120% center',
+            scrub: 5,
+        }
+    });
 });
