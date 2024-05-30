9325937a865147453da3150f2a555885556178f7
Vadimgrig2012 <Vadim.grig2012@gmail.com>
2024-05-30 09:55:28 +0400

    fix to adaptive anim h1

--- js/h1_anim.js
+++ js/h1_anim.js
@@ -1,64 +1,7 @@
-// import { gsap } from "gsap";
-
-// const tl = gsap.timeline({ repeat: -1, repeatDelay: 2.7 });
-
-// gsap.to('.h1_anim_c', {
-//     rotateZ: 1080,
-//     duration: 3,
-//     ease: 'elastic.inOut',
-//     repeat: -1,
-//     repeatDelay: 3.5
-// })
-
-
-// gsap.to('.h1_anim_i', {
-//     rotateX: 360,
-//     duration: 4,
-//     ease: 'elastic.out(1,0.3)',
-//     transformOrigin: "50% 75%",
-//     repeat: -1,
-//     repeatDelay: 2
-// })
-
-
-// tl.to('.h1_anim_l', {
-//     y: "-6rem",
-//     ease: "elastic.out(1,0.3)",
-//     duration: 0.4,
-//     stagger: 0.3
-// })
-// .to('.cat-svg', {
-//     scale: 1,
-//     opacity: 1,
-//     duration: 1,
-//     ease: 'elastic.out(1,0.3)',
-// })
-// .to('.h1_anim_l', {
-//     rotateZ: 1080,
-//     duration: 4,
-//     // ease: 'sine.inOut',
-//     ease: 'elastic.inOut',
-// })
-// .to('.cat-svg', {
-//     scale: 0,
-//     opacity: 0,
-//     duration: 1,
-//     ease: 'elastic.in(1,0.3)',
-// })
-// .to('.h1_anim_l', {
-//     delay: 0.4,
-//     y: "0rem",
-//     ease: "elastic.out(1,0.3)",
-//     duration: 0.4,
-//     stagger: 0.3
-// })
-
-
-
-
 import { gsap } from "gsap";
 
 const tl = gsap.timeline({ repeat: -1, repeatDelay: 2.7 });
+const mm = gsap.matchMedia();
 
 gsap.to('.h1_anim_c', {
     rotateZ: 1080,
@@ -77,27 +20,27 @@ gsap.to('.h1_anim_i', {
     repeatDelay: 2
 })
 
-let mm = gsap.matchMedia();
+
 
 mm.add("(max-width: 767px)", () => {
     tl.to('.h1_anim_l', {
-        y: "-2.1rem", // уменьшаем смещение по y для меньших экранов
+        y: "-2.1rem",
         ease: "elastic.out(1,0.3)",
         duration: 0.4,
         stagger: 0.3
     })
 });
 
-mm.add("(max-width: 1023px)", () => {
+mm.add("(min-width: 768px) and (max-width: 1024px)", () => {
     tl.to('.h1_anim_l', {
-        y: "-4.2rem", // уменьшаем смещение по y для меньших экранов
+        y: "-4.2rem",
         ease: "elastic.out(1,0.3)",
         duration: 0.4,
         stagger: 0.3
     })
 });
 
-mm.add("(min-width: 1024px)", () => {
+mm.add("(min-width: 1025px)", () => {
     tl.to('.h1_anim_l', {
         y: "-6rem",
         ease: "elastic.out(1,0.3)",
@@ -106,37 +49,27 @@ mm.add("(min-width: 1024px)", () => {
     })
 });
 
-
-
-tl.add(
-    gsap.to('.cat-svg', {
-        scale: 1,
-        opacity: 1,
-        duration: 1,
-        ease: 'elastic.out(1,0.3)',
-    })
-)
-.add(
-    gsap.to('.h1_anim_l', {
-        rotateZ: 1080,
-        duration: 4,
-        ease: 'elastic.inOut',
-    })
-)
-.add(
-    gsap.to('.cat-svg', {
-        scale: 0,
-        opacity: 0,
-        duration: 1,
-        ease: 'elastic.in(1,0.3)',
-    })
-)
-.add(
-    gsap.to('.h1_anim_l', {
-        delay: 0.4,
-        y: "0rem",
-        ease: "elastic.out(1,0.3)",
-        duration: 0.4,
-        stagger: 0.3
-    })
-)
+tl.to('.cat-svg', {
+    scale: 1,
+    opacity: 1,
+    duration: 1,
+    ease: 'elastic.out(1,0.3)'
+})
+.to('.h1_anim_l', {
+    rotateZ: 1080,
+    duration: 4,
+    ease: 'elastic.inOut'
+})
+.to('.cat-svg', {
+    scale: 0,
+    opacity: 0,
+    duration: 1,
+    ease: 'elastic.in(1,0.3)'
+})
+.to('.h1_anim_l', {
+    delay: 0.4,
+    y: "0rem",
+    ease: "elastic.out(1,0.3)",
+    duration: 0.4,
+    stagger: 0.3
+})
