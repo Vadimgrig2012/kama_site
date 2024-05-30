3780f215a829df197223c206816ffe4a9fea3f8d
Vadim Snopov <vadim.grig2012@gmail.com>
2024-05-30 23:58:03 +0400

    add h2 animation

---
+++ js/h2_anim.js
@@ -0,0 +1,29 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Регистрируем плагины
+gsap.registerPlugin(ScrollTrigger);
+
+// Находим элемент заголовка
+// const title = document.querySelector('.kuromi__title');
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.kuromi__title',
+    start: "top 90%",
+  }
+});
+
+tl.to('.kuromi__title', {
+    '--scaleX': '1',
+     duration: 0.5,
+     ease: 'power2.out',
+})
+.from('.kuromi__title-span', { 
+    y: '-10rem',
+    opacity: 0, 
+    ease: 'bounce.out',
+    delay: 0.1,
+    stagger: 0.2
+})
+
