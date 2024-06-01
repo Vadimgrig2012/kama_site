ee8262c20c5a504aaac77b6f23bb95d4f400b7be
Vadimgrig2012 <Vadim.grig2012@gmail.com>
2024-06-01 18:54:16 +0400

    add fix in animation scroll about

--- js/card_block_anim.js
+++ js/card_block_anim.js
@@ -1,67 +1,3 @@
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-
-// // Регистрируем плагины
-// gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-// const sections = document.querySelectorAll(".about__block");
-
-// // this scrolling object just allows us to conveniently call scrolling.enable(), scrolling.disable(), and check if scrolling.enabled is true. 
-// // some browsers (like iOS Safari) handle scrolling on a separate thread and can cause things to get out of sync (jitter/jumpy), so when we're animating the scroll position, force an update of GSAP tweens when there's a scroll event in order to maintain synchronization)
-// const scrolling = {
-//     enabled: true,
-//     events: "scroll,wheel,touchmove,pointermove".split(","),
-//     prevent: e => e.preventDefault(),
-//     disable() {
-//       if (scrolling.enabled) {
-//         scrolling.enabled = false;
-//         window.addEventListener("scroll", gsap.ticker.tick, {passive: true});
-//         scrolling.events.forEach((e, i) => (i ? document : window).addEventListener(e, scrolling.prevent, {passive: false}));
-//       }
-//     },
-//     enable() {
-//       if (!scrolling.enabled) {
-//         scrolling.enabled = true;
-//         window.removeEventListener("scroll", gsap.ticker.tick);
-//         scrolling.events.forEach((e, i) => (i ? document : window).removeEventListener(e, scrolling.prevent));
-//       }
-//     }
-//   };
-
-
-// function goToSection(section, anim, i) {
-//   if (scrolling.enabled) { // skip if a scroll tween is in progress
-//     scrolling.disable();
-//     gsap.to(window, {
-//       scrollTo: {y: section, autoKill: false},
-//       onComplete: scrolling.enable,
-//       duration: 2,
-//     });
-
-//     anim && anim.restart();
-//   }
-// }
-
-// sections.forEach((section) => {
-
-//   ScrollTrigger.create({
-//     trigger: section,
-//     start: "top bottom-=1",
-//     end: "bottom top+=1",
-//     markers: true,
-//     onEnter: () => goToSection(section),
-//     onEnterBack: () => goToSection(section)
-//   });
- 
-// });
-
-
-
-
-
-
-
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
@@ -98,7 +34,7 @@ const scrolling = {
 
 
 function goToSection(section, i) {
-  if (scrolling.enabled) { // skip if a scroll tween is in progress
+  if (scrolling.enabled) { // сбросс, если скролл в процессе
     scrolling.disable();
     gsap.to(window, {
       scrollTo: {y: section ,autoKill: false},
