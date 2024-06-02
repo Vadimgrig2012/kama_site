a57df25113b61f5ff0812f5eaed66c8b726a772b
Vadim Snopov <vadim.grig2012@gmail.com>
2024-06-02 13:46:08 +0400

    delite scrtoll animation about

--- js/card_block_anim.js
+++ js/card_block_anim.js
@@ -1,71 +1,76 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-// Регистрируем плагины
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+// // Регистрируем плагины
+// gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-const sections = document.querySelectorAll(".about__block");
-
-
-const scrolling = {
-  enabled: true,
-  events: "scroll,wheel,touchmove,pointermove".split(","),
-  prevent: e => e.preventDefault(),
-  disable() {
-    if (scrolling.enabled) {
-      scrolling.enabled = false;
-      window.addEventListener("scroll", gsap.ticker.tick, {passive: true});
-      scrolling.events.forEach((e, i) => (i ? document : window).addEventListener(e, scrolling.prevent, {passive: false}));
-    }
-  },
-  enable() {
-    if (!scrolling.enabled) {
-      scrolling.enabled = true;
-      window.removeEventListener("scroll", gsap.ticker.tick);
-      scrolling.events.forEach((e, i) => (i ? document : window).removeEventListener(e, scrolling.prevent));
-    }
-  }
-};
+// const blocks = document.querySelectorAll(".about__block");
 
 
+// const scrolling = {
+//   enabled: true,
+//   events: "scroll,wheel,touchmove,pointermove".split(","),
+//   prevent: e => e.preventDefault(),
+//   disable() {
+//     if (scrolling.enabled) {
+//       scrolling.enabled = false;
+//       window.addEventListener("scroll", gsap.ticker.tick, {passive: true});
+//       scrolling.events.forEach((e, i) => (i ? document : window).addEventListener(e, scrolling.prevent, {passive: false}));
+//     }
+//   },
+//   enable() {
+//     if (!scrolling.enabled) {
+//       scrolling.enabled = true;
+//       window.removeEventListener("scroll", gsap.ticker.tick);
+//       scrolling.events.forEach((e, i) => (i ? document : window).removeEventListener(e, scrolling.prevent));
+//     }
+//   }
+// };
 
 
 
-
-function goToSection(section, i) {
-  if (scrolling.enabled) { // сбросс, если скролл в процессе
-    scrolling.disable();
-    gsap.to(window, {
-      scrollTo: {y: section ,autoKill: false},
-      duration: 1,
-    });
-
-    gsap.from(section.querySelector('.about__card '),{
-      scaleX: 0,
-      duration: 1,
-      delay: 1,
-      transformOrigin: i === 0 ? 'right': 'left',
-      onComplete: scrolling.enable,
-    })
-    // На случай, если будем передавать анимацию из sections.forEach
-    // anim && anim.restart();
-  }
-}
+// let isAnimationInProgress = false;
 
 
+// function goToBlock(block, i) {
+//   if (scrolling.enabled) { // сбросс, если скролл в процессе
+//     isAnimationInProgress = true;
+//     scrolling.disable();
+//     gsap.to(window, {
+//       scrollTo: {y: block ,autoKill: false},
+//       duration: 1,
+//     });
+
+//     gsap.from(block.querySelector('.about__card '),{
+//       scaleX: 0,
+//       duration: 1,
+//       delay: 1,
+//       transformOrigin: i === 0 ? 'right': 'left',
+//       // onComplete: scrolling.enable,
+//       onComplete: () => {
+//         isAnimationInProgress = false;
+//         scrolling.enable();
+//       }
+//     })
+//     // На случай, если будем передавать анимацию из blocks.forEach
+//     // anim && anim.restart();
+//   }
+// }
 
 
 
-sections.forEach((section, i) => {
 
-  ScrollTrigger.create({
-    trigger: section,
-    start: "top bottom-=1",
-    end: "bottom top+=1",
-    markers: true,
-    onEnter: () => goToSection(section, i),
-    onEnterBack: () => goToSection(section, i),
-  });
 
-});
+// blocks.forEach((block, i) => {
+
+//   ScrollTrigger.create({
+//     trigger: block,
+//     start: "top bottom-=1",
+//     end: "bottom top+=1",
+//     markers: true,
+//     onEnter: () => goToBlock(block, i),
+//     onEnterBack: () => goToBlock(block, i),
+//   });
+
+// });
