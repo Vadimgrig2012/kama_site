f67100042f3c6c36a3d96e05a611c6f9ad7c398d
Vadim Snopov <vadim.grig2012@gmail.com>
2024-06-19 23:05:47 +0400

    add timeout in mainBtnAnim funck

--- js/main_btn_anim.js
+++ js/main_btn_anim.js
@@ -1,115 +1,117 @@
 import { gsap } from "gsap";
 
 
-
-const mm = gsap.matchMedia();
-mm.add("(min-width: 1025px)", () => {
-
-    const button = document.querySelector('.interactive-block__btn');
-    const txtBtn = document.querySelector('.interactive-block__btn-text')
-    const iconBtn = document.querySelector('.interactive-block__btn-icon')
-
-    const tl = gsap.timeline({ repeat: -1, yoyo: true });
-
-	tl.delay(1.5).to(button, {
-		duration: 2,
-		y: '1.5rem',
-		ease: 'sine.inOut',
-	});
-
-    button.addEventListener('mouseenter', () => {
-        // tl.pause();
-        tl.revert();
-    });
-      
-    button.addEventListener('mouseleave', () => {
-        tl.restart(true, false);
-    });
-
-    button.addEventListener('click', () => {
-        gsap.to(button, {
-            y: "0.1rem",
-            duration: 0.05,
-            repeat: 1,
-            yoyo: true
-        })
-    });
-
-
-    button.addEventListener('mousemove', (e) => {
-
-        let boundingRect = button.getBoundingClientRect();
-        let boundingRectTxt = txtBtn.getBoundingClientRect();
-
-        const mousePosX = e.clientX - boundingRect.left - boundingRect.width / 2;
-        const mousePosY = e.clientY - boundingRect.top - boundingRect.height / 2;
-
-        const yDisplacement = Math.max(-90, Math.min(90, (mousePosX / boundingRect.width) * 90));
-        const zDisplacement = Math.max(-20, Math.min(20, (mousePosX / boundingRect.width) * 20));
-
-        gsap.to(button, {
-            onStart: () => {
-                gsap.to(iconBtn, {
-                    opacity: 0,
-                    scale: 0,
-                    width: 0,
-                    display: 'none',
-                    duration: 0.1,
-                });
-                gsap.to(txtBtn, {
-                    width: 'auto',
-                    scale: 1,
-                    display: 'block',
-                    opacity: 1,
-                    duration: 0.3,
-                });
-            },
-            scale: 1.5,
-            x: mousePosX * 2,
-            y: mousePosY * 2,
-            duration: 0.5,
-            ease: 'power3.out',
+export function mainBtnAnimation () {
+    const mm = gsap.matchMedia();
+    mm.add("(min-width: 1025px)", () => {
+    
+        const button = document.querySelector('.interactive-block__btn');
+        const txtBtn = document.querySelector('.interactive-block__btn-text')
+        const iconBtn = document.querySelector('.interactive-block__btn-icon')
+    
+        const tl = gsap.timeline({ repeat: -1, yoyo: true });
+    
+        tl.delay(1.5).to(button, {
+            duration: 2,
+            y: '1.5rem',
+            ease: 'sine.inOut',
         });
-        gsap.to(txtBtn,{
-            x: mousePosX * 0.5,
-            y: mousePosY * 0.35,
-            rotationY: yDisplacement,
-            rotationZ: zDisplacement,
-            duration: 0.5,
-            ease: 'power3.out',
+    
+        button.addEventListener('mouseenter', () => {
+            // tl.pause();
+            tl.revert();
+        });
+          
+        button.addEventListener('mouseleave', () => {
+            tl.restart(true, false);
+        });
+    
+        button.addEventListener('click', () => {
+            gsap.to(button, {
+                y: "0.1rem",
+                duration: 0.05,
+                repeat: 1,
+                yoyo: true
+            })
+        });
+    
+    
+        button.addEventListener('mousemove', (e) => {
+    
+            let boundingRect = button.getBoundingClientRect();
+            let boundingRectTxt = txtBtn.getBoundingClientRect();
+    
+            const mousePosX = e.clientX - boundingRect.left - boundingRect.width / 2;
+            const mousePosY = e.clientY - boundingRect.top - boundingRect.height / 2;
+    
+            const yDisplacement = Math.max(-90, Math.min(90, (mousePosX / boundingRect.width) * 90));
+            const zDisplacement = Math.max(-20, Math.min(20, (mousePosX / boundingRect.width) * 20));
+    
+            gsap.to(button, {
+                onStart: () => {
+                    gsap.to(iconBtn, {
+                        opacity: 0,
+                        scale: 0,
+                        width: 0,
+                        display: 'none',
+                        duration: 0.1,
+                    });
+                    gsap.to(txtBtn, {
+                        width: 'auto',
+                        scale: 1,
+                        display: 'block',
+                        opacity: 1,
+                        duration: 0.3,
+                    });
+                },
+                scale: 1.5,
+                x: mousePosX * 2,
+                y: mousePosY * 2,
+                duration: 0.5,
+                ease: 'power3.out',
+            });
+            gsap.to(txtBtn,{
+                x: mousePosX * 0.5,
+                y: mousePosY * 0.35,
+                rotationY: yDisplacement,
+                rotationZ: zDisplacement,
+                duration: 0.5,
+                ease: 'power3.out',
+            });
+        });
+    
+        button.addEventListener('mouseleave', () => {
+            gsap.to(button, {
+                onStart: () => {
+                    gsap.to(iconBtn, {
+                        scale: 1,
+                        opacity: 1,
+                        width: 'auto',
+                        display: 'block',
+                        duration: 0.3,
+                        delay: 0.3,
+                        ease: 'elastic.out(1,0.3)'
+                    });
+                    gsap.to(txtBtn, {
+                        width: 0,
+                        scale: 0,
+                        display: 'none',
+                        opacity: 0,
+                        x: 0, 
+                        y: 0,
+                        rotationY: 0,
+                        rotationZ: 0,
+                        duration: 0.3,
+                    });
+                },
+                x: 0,
+                y: 0,
+                scale: 1,
+                duration: 0.7,
+                ease: 'elastic.out(1,0.3)'
+            });
         });
     });
+}
 
-    button.addEventListener('mouseleave', () => {
-        gsap.to(button, {
-            onStart: () => {
-                gsap.to(iconBtn, {
-                    scale: 1,
-                    opacity: 1,
-                    width: 'auto',
-                    display: 'block',
-                    duration: 0.3,
-                    delay: 0.3,
-                    ease: 'elastic.out(1,0.3)'
-                });
-                gsap.to(txtBtn, {
-                    width: 0,
-                    scale: 0,
-                    display: 'none',
-                    opacity: 0,
-                    x: 0, 
-                    y: 0,
-                    rotationY: 0,
-                    rotationZ: 0,
-                    duration: 0.3,
-                });
-            },
-            x: 0,
-            y: 0,
-            scale: 1,
-            duration: 0.7,
-            ease: 'elastic.out(1,0.3)'
-        });
-    });
-});
 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as mainShapesAnim from './js/main_shapes_anim'
 import * as svgLogoResize from './js/svg_logo_resize'
-import * as mainBtnAnim from './js/main_btn_anim'
+import { mainBtnAnimation } from './js/main_btn_anim'
 import * as bgCircleAnim from './js/bg_circle_anim'
 import * as h1Anim from './js/h1_anim'
 import * as ballCompositionAnim from './js/ball_composition_anim'
@@ -12,3 +12,6 @@ import * as tooltip3dAnim from './js/tooltip_3d_anim'
 import * as cursorAnim from './js/cursor_anim'
 import * as modalAnim from './js/modal_anim'
 import * as navAnim from './js/nav_anim'
+
+
+setTimeout(mainBtnAnimation, 3000);
